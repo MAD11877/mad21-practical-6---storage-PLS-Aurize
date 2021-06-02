15afca7fb3acfa964649c85365eaa6b3973c185b
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-06-02 22:47:58 +0800

    Update MyDBHandler.java

--- app/src/main/java/sg/edu/np/mad/madapplication/MyDBHandler.java
+++ app/src/main/java/sg/edu/np/mad/madapplication/MyDBHandler.java
@@ -27,26 +27,26 @@ public class MyDBHandler extends SQLiteOpenHelper {
         Log.v("created db", "on create reached!");
         String CREATE_USER_TABLE = "CREATE TABLE " + TABLE_USERS + "(" + COLUMN_NAME + " TEXT, " + COLUMN_DESCRIPTION + " TEXT, " + COLUMN_FOLLOWED + " TEXT, " + COLUMN_ID + " INTEGER PRIMARY KEY )";
         db.execSQL(CREATE_USER_TABLE);
-        db.insert(TABLE_USERS, null, addUser(new user("Reiner", "Student",1, false)));
-        db.insert(TABLE_USERS, null, addUser(new user("Luna", "Staff", 2,false)));
-        db.insert(TABLE_USERS, null,addUser(new user("Josef", "Admin",3, true)));
-        db.insert(TABLE_USERS, null,addUser(new user("Jeff", "Lecturer", 4,true)));
-        db.insert(TABLE_USERS, null,addUser(new user("David", "Student", 5,false)));
-        db.insert(TABLE_USERS, null,addUser(new user("ghost", "Student", 6,false)));
-        db.insert(TABLE_USERS, null,addUser(new user("Hazard", "Admin", 7,false)));
-        db.insert(TABLE_USERS, null,addUser(new user("Sonya", "Student",8, true)));
-        db.insert(TABLE_USERS, null,addUser(new user("Apple", "Staff",9, false)));
-        db.insert(TABLE_USERS, null,addUser(new user("Fason", "Student",10, true)));
-        db.insert(TABLE_USERS, null,addUser(new user("Mitch", "Lecturer",11, true)));
-        db.insert(TABLE_USERS, null,addUser(new user("Ern", "Lecturer",12, true)));
-        db.insert(TABLE_USERS, null,addUser(new user("Lam", "Staff",13, false)));
-        db.insert(TABLE_USERS, null,addUser(new user("Ed", "Student",14, true)));
-        db.insert(TABLE_USERS, null,addUser(new user("Ted", "Admin", 15,false)));
-        db.insert(TABLE_USERS, null,addUser(new user("Mary", "Student",16, false)));
-        db.insert(TABLE_USERS, null,addUser(new user("Blade", "Student",17, false)));
-        db.insert(TABLE_USERS, null,addUser(new user("Hale", "Staff",18, false)));
-        db.insert(TABLE_USERS, null,addUser(new user("raze", "Student",19, false)));
-        db.insert(TABLE_USERS, null,addUser(new user("Galde", "Staff",20, false)));
+        addUser(db, new user("Reiner", "Student",1, false));
+        addUser(db, new user("Luna", "Staff", 2,false));
+        addUser(db, new user("Josef", "Admin",3, true));
+        addUser(db, new user("Jeff", "Lecturer", 4,true));
+        addUser(db, new user("David", "Student", 5,false));
+        addUser(db, new user("ghost", "Student", 6,false));
+        addUser(db, new user("Hazard", "Admin", 7,false));
+        addUser(db, new user("Sonya", "Student",8, true));
+        addUser(db, new user("Apple", "Staff",9, false));
+        addUser(db, new user("Fason", "Student",10, true));
+        addUser(db, new user("Mitch", "Lecturer",11, true));
+        addUser(db, new user("Ern", "Lecturer",12, true));
+        addUser(db, new user("Lam", "Staff",13, false));
+        addUser(db, new user("Ed", "Student",14, true));
+        addUser(db, new user("Ted", "Admin", 15,false));
+        addUser(db, new user("Mary", "Student",16, false));
+        addUser(db, new user("Blade", "Student",17, false));
+        addUser(db, new user("Hale", "Staff",18, false));
+        addUser(db, new user("raze", "Student",19, false));
+        addUser(db, new user("Galde", "Staff",20, false));
     }
 
     @Override
@@ -55,40 +55,37 @@ public class MyDBHandler extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public ContentValues addUser(user user) {
+    public void addUser(SQLiteDatabase db, user user) {
         ContentValues values = new ContentValues();
         values.put(COLUMN_NAME, user.getName());
         values.put(COLUMN_DESCRIPTION, user.getDescription());
+
         boolean followed = user.isFollowed();
         String followed_string = String.valueOf(followed);
         values.put(COLUMN_FOLLOWED, followed_string);
-        values.put(COLUMN_ID, user.getId());
-        return values;
-        //SQLiteDatabase db = this.getWritableDatabase(); // get sqlitedatabase instance
-
+        db.insert(TABLE_USERS, null, values);
     }
 
     public ArrayList<user> getUser() {
         ArrayList<user> userlist = new ArrayList<user>();
-        SQLiteDatabase db = null;
+        SQLiteDatabase db = this.getReadableDatabase();
         for (int i = 0; i < 20; i++) {
             String query = "SELECT * FROM " + TABLE_USERS + " WHERE " + COLUMN_ID + "== " + i;
-            db = this.getReadableDatabase();
 
             Cursor cursor = db.rawQuery(query, null);
             user user = new user();
             if (cursor.moveToFirst()) {
                 user.setName(cursor.getString(0));
                 user.setDescription(cursor.getString(1));
-                user.setFollowed(Boolean.parseBoolean(cursor.getString(2)));
                 user.setId(Integer.parseInt(cursor.getString(3)));
+                user.setFollowed(Boolean.parseBoolean(cursor.getString(2)));
                 cursor.close();
                 userlist.add(user);
             }
             else {
                 user = null;
             }
-
+            cursor.close();
         }
         db.close();
         return userlist;
